a4c78e66f2c4c1622f66c77eb02e4846505ee5e6
Glautinor <glautinor@gmail.com>
2025-07-30 23:55:02 +0530

    Welcome to Evolution X

--- lineage_rock.mk
+++ lineage_rock.mk
@@ -28,3 +28,24 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="missi_phone_global_ext4-user 14 UP1A.231005.007 V816.0.12.0.ULUMIXM release-keys" \
     BuildFingerprint=POCO/stone_p_global/stone:13/TP1A.220624.014/V816.0.12.0.ULUMIXM:user/release-keys
+
+#Evolve
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_INCLUDE_ACCORD := false
+TARGET_ENABLE_BLUR := true
+WITH_GMS := false
+#TARGET_USES_MINI_GAPPS := true
+#TARGET_USES_PICO_GAPPS := true
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2408
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# OMX
+TARGET_SUPPORTS_OMX_SERVICE := false
+
+# Bypass charging node /sys/class/power_supply/battery/input_suspend
+BYPASS_CHARGE_SUPPORTED := false
