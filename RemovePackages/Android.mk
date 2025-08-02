2d0b1d63724650d5d0d260c7f8fd41edadb17740
Glautinor <glautinor@gmail.com>
2025-08-02 19:29:11 +0530

    removed some apps from RemovePackages/Android.mk which are needed

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,32 +4,32 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-                  AndroidAutoStubPrebuilt \
+#                  AndroidAutoStubPrebuilt \
                   AmbientSensePrebuilt \
-		  CalendarGooglePrebuilt \
-		  Camera2 \
-		  Chrome-Stub \
-		  DeskClock \
+#		   CalendarGooglePrebuilt \
+#		   Camera2 \
+#		   Chrome-Stub \
+#		   DeskClock \
                   Drive \
 		  Eleven \
 		  Etar \
-		  ExactCalculator \
+#		  ExactCalculator \
 		  Gallery2 \
 		  Jelly \
                   MaestroPrebuilt \
 		  MatLog \
-                  Maps \
-                  Gmail2 \
+#                  Maps \
+#                  Gmail2 \
 		  PersonalSafety \
-		  Photos \
-		  PrebuiltDeskClockGoogle \
+#		   Photos \
+#		   PrebuiltDeskClockGoogle \
 		  Recorder \
-                  RecorderPrebuilt \
+#                  RecorderPrebuilt \
                   SafetyHubPrebuilt \
                   Snap \
                   Turbo \
                   TipsPrebuilt \
-		  Velvet \
+#		   Velvet \
                   YouTube \
 
 LOCAL_UNINSTALLABLE_MODULE := true
