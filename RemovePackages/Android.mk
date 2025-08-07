3b8a51c0b66052550ae3acaca7df60107923f6a3
Siddharth <glautinor@gmail.com>
2025-08-08 01:55:31 +0530

    RemovePackages: Remove aosp gallery

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,33 +4,38 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-#                  AndroidAutoStubPrebuilt \
-                  AmbientSensePrebuilt \
+#       AndroidAutoStubPrebuilt \
+      AmbientSensePrebuilt \
 #		   CalendarGooglePrebuilt \
 #		   Camera2 \
 #		   Chrome-Stub \
 #		   DeskClock \
-                  Drive \
+      Drive \
 		  Eleven \
 		  Etar \
-#		  ExactCalculator \
+#		   ExactCalculator \
 		  Gallery2 \
+			Gallery \
+			Gallery3D \
+			GalleryNew3D \
+			Glimpse \
+			PhotoTable \
 		  Jelly \
-                  MaestroPrebuilt \
+      MaestroPrebuilt \
 		  MatLog \
-#                  Maps \
-#                  Gmail2 \
+#      Maps \
+#      Gmail2 \
 		  PersonalSafety \
 #		   Photos \
 #		   PrebuiltDeskClockGoogle \
 		  Recorder \
-#                  RecorderPrebuilt \
-                  SafetyHubPrebuilt \
-                  Snap \
-                  Turbo \
-                  TipsPrebuilt \
-#		   Velvet \
-                  YouTube \
+#      RecorderPrebuilt \
+      SafetyHubPrebuilt \
+      Snap \
+      Turbo \
+      TipsPrebuilt \
+#      Velvet \
+      YouTube \
 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
